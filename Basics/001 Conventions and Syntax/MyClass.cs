532c0be5e2286e6db8fb4e5fee011733cdf66e85
Samantha Zitzelsberger <moonlighticewolf@gmail.com>
2023-04-04 14:25:02 -0600

    Made a new class

--- Basics/001 Conventions and Syntax/MyClass.cs
+++ Basics/001 Conventions and Syntax/MyClass.cs
@@ -7,3 +7,28 @@
 //Add another class with any name, add variables 
 //Using the new keyword create several objects
 //Console writeline the Objects variables
+using System;
+
+public class program
+{
+    public Barbarian barbarianone;
+
+    public void Main()
+    {
+        barbarianone = new Barbarian();
+
+        barbarianone.health = 5;
+        barbarianone.powerLevel = 4;
+        barbarianone.speed = 2;
+
+        Console.WriteLine(barbarianone.health);
+        Console.WriteLine(barbarianone.powerLevel);
+        Console.WriteLine(barbarianone.speed);
+    }
+}
+
+public class Barbarian {
+    public int health;
+    public int powerLevel;
+    public int speed;
+}
